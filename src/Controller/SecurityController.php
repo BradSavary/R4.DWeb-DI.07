d5840881fb0e35274d7ae6afff6b92ed09a76645
BradSavary <brad.savary@etu.unilim.fr>
2025-03-12 16:22:00 +0100

    exercice 4 TP6 terminé (logout)

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -26,4 +26,11 @@ class SecurityController extends AbstractController
            'error'         => $error,
         ]);
     }
+    
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        // controller can be blank: it will never be executed!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
 }
